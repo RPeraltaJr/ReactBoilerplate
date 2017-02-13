c1eb0a7d9340e6931312ae1f3a8525d524fa84ee
Rafael Peralta Jr <rperaltajr@gmail.com>
2017-02-13 17:40:45 -0500

    Update app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -3,18 +3,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 // Component(s) go here
 
-var objOne = {
-  name: 'Rafael',
-  location: 'New York'
-};
-
-var objTwo = {
-  age: 27,
-  ...objOne // spread operator (...) to add all the properties of objOne
-};
-
-console.log(objTwo);
-
 ReactDOM.render(
   <h1>Boilerplate app!</h1>,
   document.getElementById('app')
